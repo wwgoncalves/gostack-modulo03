fa01c3736a2808cf5d87633fc76f56fa21285ddc
lilo <williamcco@gmail.com>
2019-09-07 13:38:12 -0300

    fix: eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
   env: {
     es6: true,
-    node: true
+    node: true,
   },
   extends: ["airbnb-base", "prettier"],
   plugins: ["prettier"],
   globals: {
     Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: "module",
   },
   rules: {
-    "prettier/prettier": "error",
+    // "prettier/prettier": "error",
     "class-methods-use-this": "off",
-    "no-param-reassing": "off",
+    "no-param-reassign": "off",
     camelcase: "off",
     "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
-    quotes: ["error", "double"]
-  }
+    quotes: ["error", "double"],
+  },
 };
