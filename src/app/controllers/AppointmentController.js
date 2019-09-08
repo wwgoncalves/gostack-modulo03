f50fe2df659133626a0bd5ba5669d335bb35027c
lilo <williamcco@gmail.com>
2019-09-08 14:41:23 -0300

    appointments table, model and endpoint for making one between users - a client and a provider

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,47 @@
+import * as Yup from "yup";
+
+import Appointment from "../models/Appointment";
+import User from "../models/User";
+
+class AppointmentController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    try {
+      if (!(await schema.isValid(request.body))) {
+        return response.status(400).json({ error: "Validation fails" });
+      }
+
+      const { provider_id, date } = request.body;
+
+      /**
+       * Check if provider_id is a provider
+       */
+
+      const isProvider = await User.findOne({
+        where: { id: provider_id, provider: true },
+      });
+
+      if (!isProvider) {
+        return response
+          .status(401)
+          .json({ error: "You can only make appointments with providers." });
+      }
+
+      const appointment = await Appointment.create({
+        user_id: request.userId,
+        provider_id,
+        date,
+      });
+
+      return response.status(201).json(appointment);
+    } catch (error) {
+      return response.status(500).json({ error });
+    }
+  }
+}
+
+export default new AppointmentController();
