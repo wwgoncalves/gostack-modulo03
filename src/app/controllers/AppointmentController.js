b30729804b053c62248ac84a5e418a750af9c393
lilo <williamcco@gmail.com>
2019-09-09 15:59:33 -0300

    appointments rules and paginated listing

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,45 @@
 import * as Yup from "yup";
+import { startOfHour, parseISO, isBefore } from "date-fns";
 
 import Appointment from "../models/Appointment";
 import User from "../models/User";
+import File from "../models/File";
 
 class AppointmentController {
+  async index(request, response) {
+    const { page = 1 } = request.query;
+    try {
+      const appointments = await Appointment.findAll({
+        where: {
+          user_id: request.userId,
+          cancelled_at: null,
+        },
+        order: ["date"],
+        attributes: ["id", "date"],
+        limit: 20,
+        offset: (page - 1) * 20,
+        include: [
+          {
+            model: User,
+            as: "provider",
+            attributes: ["id", "name"],
+            include: [
+              {
+                model: File,
+                as: "avatar",
+                attributes: ["path", "url"],
+              },
+            ],
+          },
+        ],
+      });
+
+      return response.json(appointments);
+    } catch (error) {
+      return response.status(500).json({ error });
+    }
+  }
+
   async store(request, response) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -29,6 +65,34 @@ class AppointmentController {
         return response
           .status(401)
           .json({ error: "You can only make appointments with providers." });
+      }
+
+      /**
+       * Check for past dates
+       */
+      const hourStart = startOfHour(parseISO(date));
+
+      if (isBefore(hourStart, new Date())) {
+        return response
+          .status(400)
+          .json({ error: "Past dates are forbidden." });
+      }
+
+      /**
+       * Check date availabitity
+       */
+      const checkAvailability = await Appointment.findOne({
+        where: {
+          provider_id,
+          cancelled_at: null,
+          date: hourStart,
+        },
+      });
+
+      if (checkAvailability) {
+        return response
+          .status(400)
+          .json({ error: "Appointment date is unavailable." });
       }
 
       const appointment = await Appointment.create({
