30f52691a8941aa87c0c2ea10ee1dc60c4f1d9e7
lilo <williamcco@gmail.com>
2019-09-13 11:51:59 -0300

    appointment model virtual fields

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -20,7 +20,7 @@ class AppointmentController {
           cancelled_at: null,
         },
         order: ["date"],
-        attributes: ["id", "date"],
+        attributes: ["id", "date", "past", "cancelable"],
         limit: 20,
         offset: (page - 1) * 20,
         include: [
